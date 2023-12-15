8e1546b6ae5f630c13f3b73b15de92a73d47feeb
Angel-M-R <angelmorenoromera@gmail.com>
2023-12-15 09:58:25 +0100

    hero section

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -1,6 +1,64 @@
+import "./hero.scss";
+import { motion } from "framer-motion";
+
+const sliderVariants = {
+  initial: {
+    x: 10,
+  },
+  animate: {
+    x: "-300%",
+    transition: {
+      repeat: Infinity,
+      repeatType:"mirror",
+      duration: 10,
+    }
+  },
+}
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x:0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    }
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity
+    }
+  }
+}
+
 const Hero = () => {
   return (
-    <div className='hero'>Hero</div>
+    <div className='hero'>
+      <div className="wrapper">
+        <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate">
+          <h2>ANGEL MORENO</h2>
+          <h1 >Front-end developer</h1>
+          <div className="buttons">
+            <button>See the Latest Works</button>
+            <button>Contact Me</button>
+          </div>
+          <motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt=""/>
+        </motion.div>
+      </div> 
+      <motion.div variants={sliderVariants} initial="initial" animate="animate" className="slidingTextContainer">
+        Front-end Developer 
+      </motion.div>
+      <div className="imageContainer">
+        <img src="/hero.png" alt=""/>
+      </div>
+    </div>
   )
 }
 
